0a73f76e499dfebff0089f279b6d4bf47ca845e5
Leon Huang <zlargon1988@gmail.com>
2022-11-27 07:02:50 -0500

    🔧 disable React StrictMode

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,5 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from 'components/App/App';
 
 const rootElement = document.getElementById('root') as HTMLElement;
-ReactDOM.createRoot(rootElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+ReactDOM.createRoot(rootElement).render(<App />);
