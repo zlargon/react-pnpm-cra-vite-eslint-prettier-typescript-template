cdf9d2c73b224a8c50b95046b1d0bf4d353c6a9a
Leon Huang <zlargon1988@gmail.com>
2023-07-08 13:18:58 -0400

    ♻️  [refactor] simplify store tests using Store.testUtils

--- src/store/store.test.tsx
+++ src/store/store.test.tsx
@@ -1,5 +1,4 @@
-import { renderTestStore } from './actionTestUtils';
-import { IState, Store } from './store';
+import { Store, initialState } from './store';
 import { user_click_increment_button } from './actions/user_click_increment_button';
 import { user_click_increment_button_with_number } from './actions/user_click_increment_button_with_number';
 import { initialize_application } from './asyncActions/initialize_application';
@@ -10,38 +9,28 @@ import * as API from 'apis/apis';
 jest.mock('apis/apis');
 const MockAPI = API as jest.Mocked<typeof API>;
 
+const { testDispatchActions, testDispatchAsyncActions } = Store.testUtils;
+
 test('actions', async () => {
-  type IActionNames =
-    | 'user_click_increment_button'
-    | 'user_click_increment_button_with_number'
-    | 'initialize_application'
-    | 'user_click_delay_decrement_button'
-    | 'user_click_delay_decrement_button_with_number';
-
-  const { expectState, dispatchAction } = renderTestStore<IState, IActionNames>({
-    Store,
-    actions: [user_click_increment_button],
-    actionsWithParams: [user_click_increment_button_with_number],
-    asyncActions: [initialize_application, user_click_delay_decrement_button],
-    asyncActionsWithParams: [user_click_delay_decrement_button_with_number],
-  });
-
   MockAPI.getDefaultCounter.mockResolvedValue(10);
-  dispatchAction('initialize_application');
-  await expectState((s) => {
-    expect(s.isInitializing).toBe(false);
-    expect(s.counter).toBe(10);
-  });
 
-  dispatchAction('user_click_increment_button');
-  await expectState((s) => expect(s.counter).toBe(11));
+  let state = await testDispatchAsyncActions(initialState, [initialize_application]);
 
-  dispatchAction('user_click_increment_button_with_number', 10);
-  await expectState((s) => expect(s.counter).toBe(21));
+  // user_click_increment_button
+  state = testDispatchActions(state, [user_click_increment_button]);
+  expect(state.counter).toBe(11);
 
-  dispatchAction('user_click_delay_decrement_button');
-  await expectState((s) => expect(s.counter).toBe(20));
+  // user_click_increment_button_with_number
+  state = testDispatchActions(state, [user_click_increment_button_with_number(10)]);
+  expect(state.counter).toBe(21);
 
-  dispatchAction('user_click_delay_decrement_button_with_number', 10);
-  await expectState((s) => expect(s.counter).toBe(10));
+  // async: user_click_delay_decrement_button
+  state = await testDispatchAsyncActions(state, [user_click_delay_decrement_button]);
+  expect(state.counter).toBe(20);
+
+  // async: user_click_delay_decrement_button_with_number
+  state = await testDispatchAsyncActions(state, [
+    user_click_delay_decrement_button_with_number(10),
+  ]);
+  expect(state.counter).toBe(10);
 });
